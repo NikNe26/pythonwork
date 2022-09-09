eca98d52aa65c12918919b0e716ed646b86388ac
Kab16 <nik_profe@mail.ru>
2022-09-09 16:03:01 +0300

    HW_3/1

--- main.py
+++ main.py
@@ -4,20 +4,31 @@
 #         return print(num1 / num2)
 #     else:
 #         print('Ошибка на ноль делить нельзя!')
-#
-# number1 = int(input('Введите первое число: '))
+## number1 = int(input('Введите первое число: '))
 # number2 = int(input('Введите первое число: '))
+## my_func(number1, number2)
+
+
+# def my_spr(name, surname, age, city, email, mobi):
+#     print(f'Имя: {name} Фамилия: {surname} Год рождения: {age} Город проживания: {city} Email:{email} Телефон: {mobi}')
 #
-# my_func(number1, number2)
+# n = input('Имя: ')
+# s = input('Фамилия: ')
+# a = input('Год рождения: ')
+# c = input('Город проживания: ')
+# e = input('Email: ')
+# m = input('Телефон: ')
+#
+# my_spr(n, s, a, c, e, m)
 
-def my_spr(name, surname, age, city, email, mobi):
-    print(f'Имя: {name} Фамилия: {surname} Год рождения: {age} Город проживания: {city} Email:{email} Телефон: {mobi}')
 
-name = input('Имя: ')
-surname = input('Фамилия: ')
-age = input('Год рождения: ')
-city = input('Город проживания: ')
-email = input('Email: ')
-mobi = input('Телефон: ')
+# def my_func(a, b, c):
+#     if a >= c and b >= c:
+#         print(a + b)
+#     elif a >= b and c >= b:
+#         print(a + c)
+#     else:
+#         print(b + c)
+# my_func(int(input()), int(input()), int(input()))
 
-my_spr()
+

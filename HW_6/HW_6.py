d883c2e57fc23d579e3e0447c359a43c736ac760
NikNe26 <112473240+NikNe26@users.noreply.github.com>
2022-09-19 22:37:30 +0300

    Update HW_6.py

--- HW_6/HW_6.py
+++ HW_6/HW_6.py
@@ -32,3 +32,22 @@
 # print(mas_as.get_info())
 
 # HW_6.3.
+
+# HW_6.3.
+
+# class Worker:
+#     def __init__(self, wage, bonus):
+#         self._income_ = {'wage': wage, 'bonus': bonus}
+#
+# class Position(Worker):
+#
+#     def get_full_name(self, name, surname):
+#         print(f' Фамилия: {surname} Имя: {name}')
+#
+#     def get_total_income(self, wage, bonus):
+#         super().__init__(wage, bonus)
+#         self.get_total = int(wage) + int(bonus)
+#         print(f'Доход с учетом премии: {self.get_total_income}')
+#
+# wor = Position()
+# print(wor.get_total_income(250, 30))
